6ebd1a0b120cbe9b4701a37b7f5b203671095081
wangff <1024904798@qq.com>
2020-01-02 10:29:46 +0800

    监控

--- src/main/java/com/wdcloud/config/SlaveDataSource.java
+++ src/main/java/com/wdcloud/config/SlaveDataSource.java
@@ -1,8 +1,6 @@
 package com.wdcloud.config;
 
-/**
- * Created by weitao on 2017/10/24.
- */
+
 public @interface SlaveDataSource
 {
 }
--- src/main/java/com/wdcloud/mapper/OrgEmailMapper.java
+++ src/main/java/com/wdcloud/mapper/OrgEmailMapper.java
@@ -1,6 +1,8 @@
 package com.wdcloud.mapper;
 
+import com.wdcloud.config.MasterDataSource;
 import com.wdcloud.model.OrgEmail;
 import tk.mybatis.mapper.common.Mapper;
+@MasterDataSource
 public interface OrgEmailMapper extends Mapper<OrgEmail> {
 }
