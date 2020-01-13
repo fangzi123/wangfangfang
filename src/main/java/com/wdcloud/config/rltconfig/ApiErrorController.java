35fdafa855296f240a67bcc59fafee6a83dd8319
wangff <1024904798@qq.com>
2020-01-13 18:59:45 +0800

    统一输出code

--- src/main/java/com/wdcloud/config/rltconfig/ApiErrorController.java
+++ src/main/java/com/wdcloud/config/rltconfig/ApiErrorController.java
@@ -20,14 +20,14 @@ public class ApiErrorController implements ErrorController {
         Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
         String exception_type = (String) request.getAttribute("javax.servlet.error.exception_type");
         String  message = (String) request.getAttribute("javax.servlet.error.message");
-        String  exception = (String) request.getAttribute("javax.servlet.error.exception");
+//        String  exception = (String) request.getAttribute("javax.servlet.error.exception");
         String  request_uri = (String) request.getAttribute("javax.servlet.error.request_uri");
         String  servlet_name = (String) request.getAttribute("javax.servlet.error.servlet_name");
         JSONObject json = new JSONObject();
         json.put("statusCode",statusCode);
         json.put("exception_type",exception_type);
         json.put("message",message);
-        json.put("exception",exception);
+//        json.put("exception",exception);
         json.put("request_uri",request_uri);
         json.put("servlet_name",servlet_name);
         return new CommonResult(500,"Bad Request",json);
