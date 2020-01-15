c0349c2650b8a8654f9b0fe7b1b84beba4af96ee
wangff <1024904798@qq.com>
2020-01-15 14:45:53 +0800

    filter

--- src/main/java/com/wdcloud/controller/TestController.java
+++ src/main/java/com/wdcloud/controller/TestController.java
@@ -32,6 +32,32 @@ public class TestController {
     @Autowired
     MessageSource messageSource;
 
+    /**
+     * @api {post} /app/edit 应用添加/编辑
+     * @apiDescription 应用添加
+     * @apiName appAdd
+     * @apiGroup App
+     * @apiParam {Number} [id] 应用ID
+     * @apiParam {String} name 应用名称
+     * @apiParam {String} nameCn 应用中文名称
+     * @apiParam {String} description 应用说明
+     * @apiExample {json} 请求示例:
+     * {
+     *      "id":13,
+     *      "name":"app2",
+     *      "nameCn":"应用2",
+     *      "description":"应用2应用2应用2应用2应用22应用22"
+     * }
+     * @apiSuccess {Number} code 响应码，200为处理成功，其他处理失败
+     * @apiSuccess {String} msg
+     * @apiSuccess {Number} data 新增应用ID
+     * @apiSuccessExample {json} 响应示例：
+     * {
+     *      "code": 200,
+     *      "msg": "success",
+     *      "data": 13
+     * }
+     */
     @GetMapping("/wangfangfang")
     public OrgEmail get() {
         OrgEmail orgEmail = new OrgEmail();
