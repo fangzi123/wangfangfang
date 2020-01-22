8a27f12926fa1a4bee9a3ec2f74eb3b42bc04796
wangff <1024904798@qq.com>
2020-01-22 14:52:17 +0800

    事件监听

--- src/main/java/com/wdcloud/event/AnnotationListener.java
+++ src/main/java/com/wdcloud/event/AnnotationListener.java
@@ -3,6 +3,7 @@ package com.wdcloud.event;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +17,7 @@ public class AnnotationListener {
 
     @Async
     @EventListener
+    @Order(1)
     public void eventListener(UserRegisterEvent event){
         try {
             Thread.sleep(5000);
@@ -24,5 +26,18 @@ public class AnnotationListener {
         }
         log.info("AnnotationListener================>{}", JSON.toJSONString(event.getUserBean()));
         log.info("AnnotationListener================>{}", JSON.toJSONString(event.getSource().getClass()));
+    }
+
+    @Async
+    @EventListener
+    @Order(2)
+    public void eventListener2(UserRegisterEvent event){
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("AnnotationListener2================>{}", JSON.toJSONString(event.getUserBean()));
+        log.info("AnnotationListener2================>{}", JSON.toJSONString(event.getSource().getClass()));
     }
 }
